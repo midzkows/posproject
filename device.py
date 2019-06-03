77dc9394a1d3d7df64aaf66cbb2a8248754bbe03
midzkows <idzkowska0malgorzata@gmail.com>
2019-06-03 22:06:23 +0200

    Added methods to monitor car state

--- device.py
+++ device.py
@@ -1,5 +1,40 @@
+import random
+
 
 class Device:
+    connected = {'info': '', 'state': False}
+
     def __init__(self, **kwargs):
+        """
+
+        :param kwargs:
+        """
         self.car = kwargs['car']
         self.mode = kwargs['mode']
+
+    def connect_to_car(self, state):
+        """
+
+        :param state:
+        :return:
+        """
+        self.connected = {'info': '-'.join([self.car, self.mode]), 'state': state}
+        print "Connection state: %s" % state
+
+    def measure_speed(self):
+        """
+
+        :return:
+        """
+        if self.connected['state']:
+            return {'speed': random.randint(10, 200), 'info': 'speed measured'}
+        raise RuntimeError('Failed to measure speed')
+
+    def measure_gas(self):
+        """
+
+        :return:
+        """
+        if self.connected['state']:
+            return {'gas': random.randint(10, 200), 'info': 'gas measured'}
+        raise RuntimeError('Failed to measure gas level')
--- user.py
+++ user.py
@@ -6,3 +6,19 @@ class User:
         self.mode = kwargs['mode']
         self.car = kwargs['car']
         self.device = Device(mode=self.mode, car=self.car)
+
+    speed = None
+    gas = None
+    current_position = None
+
+    def connect_to_device(self):
+        self.device.connect_to_car(True)
+
+    def disconnect_to_device(self):
+        self.device.connect_to_car(False)
+
+    def get_actual_speed(self):
+        self.speed = self.device.measure_speed()['speed']
+
+    def get_actual_gas_level(self):
+        self.gas = self.device.measure_gas()['gas']
